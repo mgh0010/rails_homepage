404330f0429e5dd48c9f8185a495db8f0b9629d5
Michaelguyson <mghharrison@gmail.com>
2018-01-10 18:14:21 +0000

    no videos

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+    def links
+    end
+    
+    def food
+    end
+    
+    def aboutme
+    end
+    
+    def finalexamcalc
+    end
+    
+    def helpfullinks
+    end
+end
